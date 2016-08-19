1c9de480a8b8cf85ca8bf2fcfe9c2d4ccd025b70
gadarohan17 <gadarohan17@gmail.com>
2016-08-19 14:03:08 +0530

    Step 1 : Chaapo

---
+++ api/controllers/KnockoutController.js
@@ -0,0 +1,127 @@
+/**
+ * KnockoutController
+ *
+ * @description :: Server-side logic for managing Knockouts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+module.exports = {
+    saveData: function(req, res) {
+        if (req.body) {
+            Knockout.saveData(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getLimited: function(req, res) {
+        if (req.body) {
+            if (req.body.pagenumber) {
+                Knockout.findLimited(req.body, res.callback);
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Params"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+    getAll: function(req, res) {
+        if (req.body) {
+            Knockout.getAll(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    deleteData: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id !== "") {
+                Knockout.deleteData(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getOne: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id !== "") {
+                Knockout.getOne(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    }
+};
---
+++ api/services/Knockout.js
@@ -0,0 +1,192 @@
+/**
+ * Booking.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+var Schema = sails.mongoose.Schema;
+var schema = new Schema({
+  year:String,
+    school: {
+        type: Schema.Types.ObjectId,
+        ref: "School",
+        index: true
+    },
+    name: String,
+    sport: {
+        type: Schema.Types.ObjectId,
+        ref: "SportsList",
+        index: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: "FirstCategory",
+        index: true
+    },
+    agegroup: {
+        type: Schema.Types.ObjectId,
+        ref: "Agegroup",
+        index: true
+    },
+    captain: {
+        type: Schema.Types.ObjectId,
+        ref: "Student",
+        index: true
+    },
+    gender: String,
+    coach: String,
+    players: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+        index: true
+    }]
+});
+module.exports = sails.mongoose.model('Knockout', schema);
+var models = {
+    saveData: function(data, callback) {
+        var knockout = this(data);
+        var matchObj = {};
+        if (data.category && data.category === "") {
+            delete data.category;
+            matchObj = {
+                school: data.school,
+                sport: data.sport,
+                agegroup: data.agegroup,
+                gender: data.gender
+            };
+        } else {
+            matchObj = {
+                school: data.school,
+                sport: data.sport,
+                agegroup: data.agegroup,
+                gender: data.gender,
+                category: data.category
+            };
+        }
+        console.log(matchObj);
+        if (data._id) {
+            this.findOneAndUpdate({
+                _id: data._id
+            }, data, function(err, data2) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, data2);
+                }
+            });
+        } else {
+            Knockout.update(matchObj, {
+                $set: {
+                    name: data.name + " 'A'"
+                }
+            }).exec(function(err, updated) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else if (updated.nModified > 0) {
+                    knockout.name = data.name + " 'B'";
+                    knockout.save(function(err, data2) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            callback(null, data2);
+                        }
+                    });
+                } else {
+                    knockout.save(function(err, data2) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            callback(null, data2);
+                        }
+                    });
+                }
+            });
+        }
+    },
+    getAll: function(data, callback) {
+        Knockout.find({}, {}, {}, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, deleted);
+            }
+        });
+    },
+    findLimited : function (data,callback) {
+      var newreturns = {};
+      newreturns.data = [];
+      data.pagenumber = parseInt(data.pagenumber);
+      var checkObj = {};
+
+          var check = new RegExp(data.name, "i");
+          checkObj = {
+              name: {
+                  '$regex': check
+              }
+          };
+
+      async.parallel([
+              function(callback) {
+                  Knockout.count(checkObj).exec(function(err, number) {
+                      if (err) {
+                          console.log(err);
+                          callback(err, null);
+                      } else if (number && number !== "") {
+                          newreturns.total = number;
+                          newreturns.totalpages = Math.ceil(number / 20);
+                          callback(null, newreturns);
+                      } else {
+                          callback(null, newreturns);
+                      }
+                  });
+              },
+              function(callback) {
+                  Knockout.find(checkObj).sort().skip(20 * (data.pagenumber - 1)).limit(20).populate('players',"_id name ").populate("school","name").populate('sport',"name").populate("agegroup","name").populate("category","name").exec(function(err, data2) {
+                      if (err) {
+                          console.log(err);
+                          callback(err, null);
+                      } else if (data2 && data2.length > 0) {
+                          newreturns.data = data2;
+                          callback(null, newreturns);
+                      } else {
+                          callback(null, newreturns);
+                      }
+                  });
+              }
+          ],
+          function(err, data4) {
+              if (err) {
+                  console.log(err);
+                  callback(err, null);
+              } else if (data4) {
+                  callback(null, newreturns);
+              } else {
+                  callback(null, newreturns);
+              }
+          });
+    },
+    deleteData: function(data, callback) {
+        Knockout.findOneAndRemove({
+            _id: data._id
+        }, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, deleted);
+            }
+        });
+    },
+    getOne: function(data, callback) {
+        Knockout.findOne({
+            _id: data._id
+        }, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, deleted);
+            }
+        }).populate('players',"_id name sfaid").populate('captain',"_id name sfaid").populate("school","name sfaid").populate('sport',"name").populate("agegroup","name").populate("category","name");
+    }
+};
+module.exports = _.assign(module.exports, models);
