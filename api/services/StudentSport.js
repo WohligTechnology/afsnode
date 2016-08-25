36d650b25c437a2612b32dace58ec8686722f7fd
gadarohan17 <gadarohan17@gmail.com>
2016-08-25 15:09:14 +0530

    search student by sport done

--- api/services/StudentSport.js
+++ api/services/StudentSport.js
@@ -6,176 +6,201 @@
  */
 var Schema = sails.mongoose.Schema;
 var schema = new Schema({
-    year: String,
-    sportslist: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String,
-            sporttype: String
-        }
-    },
-    agegroup: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String
-        }
-    },
-    firstcategory: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String
-        }
-    },
-    secondcategory: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String
-        }
-    },
-    thirdcategory: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String
-        }
-    },
-    medals: Schema.Types.Mixed,
-    school: {
-        type: {
-            _id: {
-                type: Schema.Types.ObjectId
-            },
-            name: String
-        }
-    },
-    student: {
-        type: Schema.Types.ObjectId,
-        ref:'Student'
+  year: String,
+  sportslist: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String,
+      sporttype: String
     }
+  },
+  agegroup: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String
+    }
+  },
+  firstcategory: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String
+    }
+  },
+  secondcategory: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String
+    }
+  },
+  thirdcategory: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String
+    }
+  },
+  medals: Schema.Types.Mixed,
+  school: {
+    type: {
+      _id: {
+        type: Schema.Types.ObjectId
+      },
+      name: String
+    }
+  },
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: 'Student'
+  }
 });
 module.exports = sails.mongoose.model('StudentSport', schema);
 var models = {
-    saveData: function(data, callback) {
-        var studentsport = this(data);
-        if (data._id) {
-            this.findOneAndUpdate({
-                _id: data._id
-            }, data, function(err, data2) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, data2);
-                }
-            });
+  saveData: function(data, callback) {
+    var studentsport = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, function(err, data2) {
+        if (err) {
+          callback(err, null);
         } else {
-            studentsport.save(function(err, data2) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, data2);
-                }
-            });
+          callback(null, data2);
         }
-    },
-    getAll: function(data, callback) {
-        StudentSport.find({}, {}, {}, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    deleteData: function(data, callback) {
-        StudentSport.findOneAndRemove({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    getOne: function(data, callback) {
-        StudentSport.findOne({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    getStudentsbySport :function (data,callback) {
-      // StudentSport.find({
-      //     'sportslist._id': data.sport
-      // },{
-      //   _id:0,
-      //   student:1
-      // }, function(err, deleted) {
-      //     if (err) {
-      //         callback(err, null);
-      //     } else {
-      //         callback(null, deleted);
-      //     }
-      // }).populate("student");
-
-      StudentSport.aggregate([{
-          $match: {
-             'sportslist._id': data.sport,
-             'year':data.year
-          }
-      }, {
-          $project: {
-            _id:0,
-            student:1
-          }
-      }]).exec(function(err, data2) {
-        console.log(data2);
-          if (err) {
-              console.log(err);
-              callback(err, null);
-          } else if (data2 && data2.length > 0) {
-            StudentSport.populate(data2,{
-              path:"student"
-            },function (err,data) {
-              if(err){
-                callback(err, null);
-              }else{
-                data = _.map(data,function (key) {
-                  return key.student;
-                });
-                callback(null, data);
-
-              }
-            });
-          } else {
-              callback({}, null);
-          }
       });
-    },
-
-    getSports: function(data, callback) {
-        StudentSport.find({
-            student: data.student
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
+    } else {
+      studentsport.save(function(err, data2) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, data2);
+        }
+      });
     }
+  },
+  getAll: function(data, callback) {
+    StudentSport.find({}, {}, {}, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  deleteData: function(data, callback) {
+    StudentSport.findOneAndRemove({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  getOne: function(data, callback) {
+    StudentSport.findOne({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  getStudentsbySport: function(data, callback) {
+    // StudentSport.find({
+    //     'sportslist._id': data.sport
+    // },{
+    //   _id:0,
+    //   student:1
+    // }, function(err, deleted) {
+    //     if (err) {
+    //         callback(err, null);
+    //     } else {
+    //         callback(null, deleted);
+    //     }
+    // }).populate("student");
+    if(!data.search){
+      data.search = "";
+    }{
+      data.search = new RegExp(data.search, "i");
+    }
+    StudentSport.aggregate([{
+      $match: {
+        'sportslist._id': data.sport,
+        'year': data.year
+      }
+    }, {
+      $lookup: {
+        from: 'students',
+        localField: 'student',
+        foreignField: '_id',
+        as: 'student'
+      }
+    },{
+      $unwind:'$student'
+    },{
+      $match :{
+        'student.name':{
+          '$regex':data.search
+        }
+      }
+    }, {
+      $project: {
+        _id: 0,
+        student: 1
+      }
+    }, {
+      $limit: 10
+    }]).exec(function(err, data2) {
+      // console.log(data2);
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      } else if (data2 && data2.length > 0) {
+        // StudentSport.populate(data2,{
+        //   path:"student"
+        // },function (err,data) {
+        //   if(err){
+        //     callback(err, null);
+        //   }else{
+        //     data = _.map(data,function (key) {
+        //       return key.student;
+        //     });
+        //     callback(null, data);
+        //
+        //   }
+        // });
+            data2 = _.map(data2,function (key) {
+              return key.student;
+            });
+        callback(null, data2);
+      } else {
+        callback({}, null);
+      }
+    });
+  },
+
+  getSports: function(data, callback) {
+    StudentSport.find({
+      student: data.student
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  }
 };
 module.exports = _.assign(module.exports, models);
