2220e1f28762e0df6a7f7ca1cc672b86faf4703f
gadarohan17 <gadarohan17@gmail.com>
2016-09-12 20:46:50 +0530

    changes

--- api/services/StudentSport.js
+++ api/services/StudentSport.js
@@ -247,6 +247,90 @@ var models = {
       }
     });
   },
+  getStudentsbySportwithExclude: function(data, callback) {
+    //For Knockout
+    // StudentSport.find({
+    //     'sportslist._id': data.sport
+    // },{
+    //   _id:0,
+    //   student:1
+    // }, function(err, deleted) {
+    //     if (err) {
+    //         callback(err, null);
+    //     } else {
+    //         callback(null, deleted);
+    //     }
+    // }).populate("student");
+    var studentconstraints = {};
+    if(data.sfaid){
+      studentconstraints ={
+        $match :{
+          'student.sfaid':data.sfaid
+        }
+      };
+    }else{
+      data.search = new RegExp(data.search, "i");
+      studentconstraints = {
+        $match :{
+          'student.name':{
+            '$regex':data.search
+          }
+        }
+      };
+    }
+    if(data.gender){
+      studentconstraints.$match['student.gender'] = data.gender;
+    }
+    console.log(studentconstraints);
+    StudentSport.aggregate([{
+      $match: {
+        'sportslist._id': data.sport,
+        'year': data.year
+      }
+    }, {
+      $lookup: {
+        from: 'students',
+        localField: 'student',
+        foreignField: '_id',
+        as: 'student'
+      }
+    },{
+      $unwind:'$student'
+    },studentconstraints, {
+      $project: {
+        _id: 0,
+        student: 1
+      }
+    }, {
+      $limit: 10
+    }]).exec(function(err, data2) {
+      // console.log(data2);
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      } else if (data2 && data2.length > 0) {
+        // StudentSport.populate(data2,{
+        //   path:"student"
+        // },function (err,data) {
+        //   if(err){
+        //     callback(err, null);
+        //   }else{
+        //     data = _.map(data,function (key) {
+        //       return key.student;
+        //     });
+        //     callback(null, data);
+        //
+        //   }
+        // });
+            data2 = _.map(data2,function (key) {
+              return key.student;
+            });
+        callback(null, data2);
+      } else {
+        callback({}, null);
+      }
+    });
+  },
 
   getSports: function(data, callback) {
     StudentSport.find({
