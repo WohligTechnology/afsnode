8a748e2b76709b74349c275a4b1e7ec94f965ae2
gadarohan17 <gadarohan17@gmail.com>
2016-09-13 11:56:35 +0530

    notpaidfor

--- api/services/School.js
+++ api/services/School.js
@@ -69,6 +69,7 @@ var schema = new Schema({
         contact: String,
         email: String
     }],
+    notpaidfor : String,
     timestamp: Date,
     representative: String,
     numberOfSports: String,
