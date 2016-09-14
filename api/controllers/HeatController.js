9d2da0c6cef3278c83ba46a4b55a9feb83b9b60f
gadarohan17 <gadarohan17@gmail.com>
2016-09-14 12:33:10 +0530

    Heater start

---
+++ api/controllers/HeatController.js
@@ -0,0 +1,139 @@
+/**
+ * HeatController
+ *
+ * @description :: Server-side logic for managing Heats
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+module.exports = {
+    saveData: function(req, res) {
+        if (req.body) {
+            Heat.saveData(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getAll: function(req, res) {
+        if (req.body) {
+            Heat.getAll(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    deleteData: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id != "") {
+                Heat.deleteData(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getOne: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id != "") {
+                Heat.getOne(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    findForDrop: function(req, res) {
+        if (req.body) {
+            if (req.body.thirdcategory && Array.isArray(req.body.thirdcategory)) {
+                Heat.findForDrop(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide parameters"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Please provide parameters"
+            });
+        }
+    }
+};
---
+++ api/services/Heat.js
@@ -0,0 +1,133 @@
+/**
+ * Booking.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+var Schema = sails.mongoose.Schema;
+var schema = new Schema({
+  year: String,
+  matchid: Number,
+  sport: {
+    type: Schema.Types.ObjectId,
+    ref: "Sport",
+    index: true
+  },
+  event: {
+    type: String,
+    default: "Heats"
+  },
+  participantType: {
+    type: String,
+    default: "player"
+  }
+});
+module.exports = sails.mongoose.model('Heat', schema);
+var models = {
+  saveData: function(data, callback) {
+    var heat = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, function(err, data2) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, data2);
+        }
+      });
+    } else {
+      Heat.find({
+        "name": data.name
+      }).exec(function(err, data2) {
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else if (data2 && data2[0]) {
+          callback(null, data2);
+        } else {
+          heat.save(function(err, data3) {
+            if (err) {
+              callback(err, null);
+            } else {
+              callback(null, data3);
+            }
+          });
+        }
+      });
+    }
+  },
+  getAll: function(data, callback) {
+    Heat.find({}, {}, {}, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  deleteData: function(data, callback) {
+    Heat.findOneAndRemove({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null)
+      } else {
+        callback(null, deleted)
+      }
+    });
+  },
+  getOne: function(data, callback) {
+    Heat.findOne({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  findForDrop: function(data, callback) {
+    var returns = [];
+    var exit = 0;
+    var exitup = 1;
+    var check = new RegExp(data.search, "i");
+
+    function callback2(exit, exitup, data) {
+      if (exit == exitup) {
+        callback(null, data);
+      }
+    }
+    Heat.find({
+      name: {
+        '$regex': check
+      }
+    }).limit(10).exec(function(err, found) {
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      }
+      if (found && found.length > 0) {
+        exit++;
+        if (data.heat.length != 0) {
+          var nedata;
+          nedata = _.remove(found, function(n) {
+            var flag = false;
+            _.each(data.heat, function(n1) {
+              if (n1.name == n.name) {
+                flag = true;
+              }
+            })
+            return flag;
+          });
+        }
+        returns = returns.concat(found);
+        callback2(exit, exitup, returns);
+      } else {
+        callback([], null);
+      }
+    });
+  }
+};
+module.exports = _.assign(module.exports, models);
