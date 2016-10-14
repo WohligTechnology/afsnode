3dc8962accf1b114c59776ee53700039177cf9f2
gadarohan17 <gadarohan17@gmail.com>
2016-10-14 18:07:14 +0530

    Bulk save

--- api/controllers/BulkController.js
+++ api/controllers/BulkController.js
@@ -1,3 +1,34 @@
 module.exports = {
-  
+  sportlist: function(req,res) {
+    StudentSport.find().exec(function(err,data) {
+      if(err) {
+        res.json(err);
+      }
+      else {
+        console.log("Total Length "+ data.length);
+        _.each(data,function(n,key) {
+          n.save(function() {console.log(key+ " Done");});
+
+        });
+        res.json({done:"Done"});
+      }
+
+    });
+  },
+  sport: function(req,res) {
+    Sport.find().exec(function(err,data) {
+      if(err) {
+        res.json(err);
+      }
+      else {
+        console.log("Total Length "+ data.length);
+        _.each(data,function(n,key) {
+          n.save(function() {console.log(key+ " Done");});
+
+        });
+        res.json({done:"Done"});
+      }
+
+    });
+  }
 };
