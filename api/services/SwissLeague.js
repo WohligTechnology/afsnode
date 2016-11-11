ff84b56ff6f63739bf9c11e992ceda90b774ee92
gadarohan17 <gadarohan17@gmail.com>
2016-11-11 16:49:26 +0530

    chnages

--- api/services/SwissLeague.js
+++ api/services/SwissLeague.js
@@ -8,7 +8,7 @@ var Schema = sails.mongoose.Schema;
 var schema = new Schema({
   year: String,
   matchid: Number,
-order: {
+  order: {
     type: Number,
     default: 0
   },
@@ -20,13 +20,13 @@ order: {
   event: {
     type: String,
     default: "Swiss League"
-  },;
+  },
   participantType: {
     type: String,
     default: "player"
   },
-  round:{
-    type:String
+  round: {
+    type: String
   },
   date: {
     type: Date,
@@ -49,10 +49,10 @@ order: {
   point2: {
     type: Number,
   },
-no1: {
+  no1: {
     type: Number,
   },
-no2: {
+  no2: {
     type: Number,
   },
   video: {
@@ -61,110 +61,110 @@ no2: {
 });
 module.exports = sails.mongoose.model('SwissLeague', schema);
 var models = {
-    saveData: function(data, callback) {
-        var swissleague = this(data);
-        if (data._id) {
-            this.findOneAndUpdate({
-                _id: data._id
-            }, data, function(err, data2) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, data2);
-                }
-            });
+  saveData: function(data, callback) {
+    var swissleague = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, function(err, data2) {
+        if (err) {
+          callback(err, null);
         } else {
-            SwissLeague.find({
-                "name": data.name
-            }).exec(function(err, data2) {
-                if (err) {
-                    console.log(err);
-                    callback(err, null);
-                } else if (data2 && data2[0]) {
-                    callback(null, data2);
-                } else {
-                    swissleague.save(function(err, data3) {
-                        if (err) {
-                            callback(err, null);
-                        } else {
-                            callback(null, data3);
-                        }
-                    });
-                }
-            });
+          callback(null, data2);
         }
-    },
-    getAll: function(data, callback) {
-        SwissLeague.find({}, {}, {}, function(err, deleted) {
+      });
+    } else {
+      SwissLeague.find({
+        "name": data.name
+      }).exec(function(err, data2) {
+        if (err) {
+          console.log(err);
+          callback(err, null);
+        } else if (data2 && data2[0]) {
+          callback(null, data2);
+        } else {
+          swissleague.save(function(err, data3) {
             if (err) {
-                callback(err, null);
+              callback(err, null);
             } else {
-                callback(null, deleted);
+              callback(null, data3);
             }
-        });
-    },
-    deleteData: function(data, callback) {
-        SwissLeague.findOneAndRemove({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    getOne: function(data, callback) {
-        SwissLeague.findOne({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    findForDrop: function(data, callback) {
-        var returns = [];
-        var exit = 0;
-        var exitup = 1;
-        var check = new RegExp(data.search, "i");
-
-        function callback2(exit, exitup, data) {
-            if (exit == exitup) {
-                callback(null, data);
-            }
+          });
         }
-        SwissLeague.find({
-            name: {
-                '$regex': check
-            }
-        }).limit(10).exec(function(err, found) {
-            if (err) {
-                console.log(err);
-                callback(err, null);
-            }
-            if (found && found.length > 0) {
-                exit++;
-                if (data.swissleague.length !== 0) {
-                    var nedata;
-                    nedata = _.remove(found, function(n) {
-                        var flag = false;
-                        _.each(data.swissleague, function(n1) {
-                            if (n1.name == n.name) {
-                                flag = true;
-                            }
-                        });
-                        return flag;
-                    });
-                }
-                returns = returns.concat(found);
-                callback2(exit, exitup, returns);
-            } else {
-                callback([], null);
-            }
-        });
+      });
     }
+  },
+  getAll: function(data, callback) {
+    SwissLeague.find({}, {}, {}, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  deleteData: function(data, callback) {
+    SwissLeague.findOneAndRemove({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  getOne: function(data, callback) {
+    SwissLeague.findOne({
+      _id: data._id
+    }, function(err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  findForDrop: function(data, callback) {
+    var returns = [];
+    var exit = 0;
+    var exitup = 1;
+    var check = new RegExp(data.search, "i");
+
+    function callback2(exit, exitup, data) {
+      if (exit == exitup) {
+        callback(null, data);
+      }
+    }
+    SwissLeague.find({
+      name: {
+        '$regex': check
+      }
+    }).limit(10).exec(function(err, found) {
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      }
+      if (found && found.length > 0) {
+        exit++;
+        if (data.swissleague.length !== 0) {
+          var nedata;
+          nedata = _.remove(found, function(n) {
+            var flag = false;
+            _.each(data.swissleague, function(n1) {
+              if (n1.name == n.name) {
+                flag = true;
+              }
+            });
+            return flag;
+          });
+        }
+        returns = returns.concat(found);
+        callback2(exit, exitup, returns);
+      } else {
+        callback([], null);
+      }
+    });
+  }
 };
 module.exports = _.assign(module.exports, models);
