c43ed2fcecdb117b5df04a6ff80129549225ba06
gadarohan17 <gadarohan17@gmail.com>
2016-11-23 17:25:53 +0530

    API start

--- api/controllers/QualifyingRoundController.js
+++ api/controllers/QualifyingRoundController.js
@@ -1,11 +1,139 @@
 /**
  * QualifyingRoundController
  *
- * @description :: Server-side logic for managing qualifyingrounds
+ * @description :: Server-side logic for managing QualifyingRounds
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
 module.exports = {
-	
+    saveData: function(req, res) {
+        if (req.body) {
+            QualifyingRound.saveData(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getAll: function(req, res) {
+        if (req.body) {
+            QualifyingRound.getAll(req.body, function(err, respo) {
+                if (err) {
+                    res.json({
+                        value: false,
+                        data: err
+                    });
+                } else {
+                    res.json({
+                        value: true,
+                        data: respo
+                    });
+                }
+            });
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    deleteData: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id !== "") {
+                QualifyingRound.deleteData(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    getOne: function(req, res) {
+        if (req.body) {
+            if (req.body._id && req.body._id !== "") {
+                QualifyingRound.getOne(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Invalid Id"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid call"
+            });
+        }
+    },
+    findForDrop: function(req, res) {
+        if (req.body) {
+            if (req.body.firstcategory && Array.isArray(req.body.firstcategory)) {
+                QualifyingRound.findForDrop(req.body, function(err, respo) {
+                    if (err) {
+                        res.json({
+                            value: false,
+                            data: err
+                        });
+                    } else {
+                        res.json({
+                            value: true,
+                            data: respo
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    value: false,
+                    data: "Please provide parameters"
+                });
+            }
+        } else {
+            res.json({
+                value: false,
+                data: "Please provide parameters"
+            });
+        }
+    }
 };
-
--- api/services/QualifyingRound.js
+++ api/services/QualifyingRound.js
@@ -4,11 +4,116 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var Schema = sails.mongoose.Schema;
+var schema = new Schema({
+    name: String
+});
+module.exports = sails.mongoose.model('QualifyingRound', schema);
+var models = {
+    saveData: function(data, callback) {
+        var qualifyinground = this(data);
+        if (data._id) {
+            this.findOneAndUpdate({
+                _id: data._id
+            }, data, function(err, data2) {
+                if (err) {
+                    callback(err, null);
+                } else {
+                    callback(null, data2);
+                }
+            });
+        } else {
+            QualifyingRound.find({
+                "name": data.name
+            }).exec(function(err, data2) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else if (data2 && data2[0]) {
+                    callback(null, data2);
+                } else {
+                    qualifyinground.save(function(err, data3) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            callback(null, data3);
+                        }
+                    });
+                }
+            });
+        }
+    },
+    getAll: function(data, callback) {
+        QualifyingRound.find({}, {}, {}, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, deleted);
+            }
+        });
+    },
+    deleteData: function(data, callback) {
+        QualifyingRound.findOneAndRemove({
+            _id: data._id
+        }, function(err, deleted) {
+            if (err) {
+                callback(err, null)
+            } else {
+                callback(null, deleted)
+            }
+        });
+    },
+    getOne: function(data, callback) {
+        QualifyingRound.findOne({
+            _id: data._id
+        }, function(err, deleted) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, deleted);
+            }
+        });
+    },
+    findForDrop: function(data, callback) {
+        var returns = [];
+        var exit = 0;
+        var exitup = 1;
+        var check = new RegExp(data.search, "i");
 
-module.exports = {
-
-  attributes: {
-
-  }
+        function callback2(exit, exitup, data) {
+            if (exit == exitup) {
+                callback(null, data);
+            }
+        }
+        QualifyingRound.find({
+            name: {
+                '$regex': check
+            }
+        }).limit(10).exec(function(err, found) {
+            if (err) {
+                console.log(err);
+                callback(err, null);
+            }
+            if (found && found.length > 0) {
+                exit++;
+                if (data.qualifyinground.length !== 0) {
+                    var nedata;
+                    nedata = _.remove(found, function(n) {
+                        var flag = false;
+                        _.each(data.qualifyinground, function(n1) {
+                            if (n1.name == n.name) {
+                                flag = true;
+                            }
+                        });
+                        return flag;
+                    });
+                }
+                returns = returns.concat(found);
+                callback2(exit, exitup, returns);
+            } else {
+                callback([], null);
+            }
+        });
+    }
 };
-
+module.exports = _.assign(module.exports, models);
