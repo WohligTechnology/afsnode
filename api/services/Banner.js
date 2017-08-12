a4c03f633daa05d5f3d9ee9e29115459d73d23d3
Rajmshah <rajwohlig@gmail.com>
2017-08-12 19:55:45 +0530

    to test

--- api/services/Banner.js
+++ api/services/Banner.js
@@ -6,130 +6,153 @@
  */
 var Schema = sails.mongoose.Schema;
 var schema = new Schema({
-  order:{
-    type:Number,
-    default:0
+  order: {
+    type: Number,
+    default: 0
   },
-  image:{
-    type:String,
-    default:""
+  image: {
+    type: String,
+    default: ""
   },
-  link:{
-    type:String,
-    default:""
+  link: {
+    type: String,
+    default: ""
   },
-  status:{
-    type:Boolean,
-    default:true
+  city: {
+    type: String,
+    default: ""
+  },
+  status: {
+    type: Boolean,
+    default: true
   }
 });
 module.exports = sails.mongoose.model('Banner', schema);
 var models = {
-    saveData: function(data, callback) {
-        var banner = this(data);
-        if (data._id) {
-            this.findOneAndUpdate({
-                _id: data._id
-            }, data, function(err, data2) {
-                if (err) {
-                    callback(err, null);
-                } else {
-                    callback(null, data2);
-                }
-            });
+  saveData: function (data, callback) {
+    var banner = this(data);
+    if (data._id) {
+      this.findOneAndUpdate({
+        _id: data._id
+      }, data, function (err, data2) {
+        if (err) {
+          callback(err, null);
         } else {
-                    banner.save(function(err, data3) {
-                        if (err) {
-                            callback(err, null);
-                        } else {
-                            callback(null, data3);
-                        }
-                    });
-
+          callback(null, data2);
         }
-    },
-    getAll: function(data, callback) {
-        Banner.find({}, {}, {}, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    getAllEnabledBanner: function(data, callback) {
-        Banner.find({
-          status:true
-        }, {}, {}, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    deleteData: function(data, callback) {
-        Banner.findOneAndRemove({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    getOne: function(data, callback) {
-        Banner.findOne({
-            _id: data._id
-        }, function(err, deleted) {
-            if (err) {
-                callback(err, null);
-            } else {
-                callback(null, deleted);
-            }
-        });
-    },
-    findForDrop: function(data, callback) {
-        var returns = [];
-        var exit = 0;
-        var exitup = 1;
-        var check = new RegExp(data.search, "i");
-
-        function callback2(exit, exitup, data) {
-            if (exit == exitup) {
-                callback(null, data);
-            }
+      });
+    } else {
+      banner.save(function (err, data3) {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, data3);
         }
-        Banner.find({
-            name: {
-                '$regex': check
-            }
-        }).limit(10).exec(function(err, found) {
-            if (err) {
-                console.log(err);
-                callback(err, null);
-            }
-            if (found && found.length > 0) {
-                exit++;
-                if (data.banner.length != 0) {
-                    var nedata;
-                    nedata = _.remove(found, function(n) {
-                        var flag = false;
-                        _.each(data.banner, function(n1) {
-                            if (n1.name == n.name) {
-                                flag = true;
-                            }
-                        })
-                        return flag;
-                    });
-                }
-                returns = returns.concat(found);
-                callback2(exit, exitup, returns);
-            } else {
-                callback([], null);
-            }
-        });
+      });
+
     }
+  },
+  getAll: function (data, callback) {
+    Banner.find({}, {}, {}, function (err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  getAllEnabledBanner: function (data, callback) {
+    async.waterfall([
+      function (callback) {
+        Banner.find({
+          status: true
+        }, {}, {}, function (err, deleted) {
+          if (err) {
+            callback(err, null);
+          } else {
+            callback(null, deleted);
+          }
+        });
+      },
+      function (deleted, callback) {
+        var results = _.groupBy(deleted, "city");
+        callback(null, results);
+      }
+    ], function (err, results) {
+      if (err) {
+        console.log(err);
+        callback(null, []);
+      } else if (results) {
+        if (_.isEmpty(results)) {
+          callback(null, []);
+        } else {
+          callback(null, results);
+        }
+      }
+    });
+  },
+  deleteData: function (data, callback) {
+    Banner.findOneAndRemove({
+      _id: data._id
+    }, function (err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  getOne: function (data, callback) {
+    Banner.findOne({
+      _id: data._id
+    }, function (err, deleted) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, deleted);
+      }
+    });
+  },
+  findForDrop: function (data, callback) {
+    var returns = [];
+    var exit = 0;
+    var exitup = 1;
+    var check = new RegExp(data.search, "i");
+
+    function callback2(exit, exitup, data) {
+      if (exit == exitup) {
+        callback(null, data);
+      }
+    }
+    Banner.find({
+      name: {
+        '$regex': check
+      }
+    }).limit(10).exec(function (err, found) {
+      if (err) {
+        console.log(err);
+        callback(err, null);
+      }
+      if (found && found.length > 0) {
+        exit++;
+        if (data.banner.length != 0) {
+          var nedata;
+          nedata = _.remove(found, function (n) {
+            var flag = false;
+            _.each(data.banner, function (n1) {
+              if (n1.name == n.name) {
+                flag = true;
+              }
+            })
+            return flag;
+          });
+        }
+        returns = returns.concat(found);
+        callback2(exit, exitup, returns);
+      } else {
+        callback([], null);
+      }
+    });
+  }
 };
 module.exports = _.assign(module.exports, models);
